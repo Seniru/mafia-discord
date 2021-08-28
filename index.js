286abbd574c504895b137bf0f334033cc39a45fc
Seniru Pasan Indira <senirupasan@gmail.com>
2021-08-28 10:52:55 +0000

    chore: Very basic setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const discord = require("discord.js")
+require("dotenv").config()
+
+// Create a new client instance
+const client = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+client.login(process.env.TOKEN);
